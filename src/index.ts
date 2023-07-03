b5449e38a6778fb85ec7e9265448c1a4c209bce6
Daniel <daniel@sohe.ir>
2023-07-03 09:44:06 +0330

    chore: removed things we don't need

--- src/index.ts
+++ src/index.ts
@@ -1,25 +1,8 @@
 import {
   TObject,
-  TProperties,
   TSchema,
-  TString,
   Type,
 } from "@sinclair/typebox";
-import { Value } from "@sinclair/typebox/value";
-import * as assert from "assert";
-
-const filterAbles = ["email", "phone"];
-
-type filterStructure =
-  | {
-      $and: filterStructure[];
-    }
-  | {
-      $or: filterStructure[];
-    }
-  | {
-      [key: string]: TString;
-    };
 
 const fields = (ables: string[]) => {
   const ablesSchema: TObject[] = [];
@@ -66,8 +49,3 @@ export const filterSchema = (ables: string[]) => {
   if (!schema) throw new Error("this is not possible");
   return Type.Optional(schema);
 };
-
-const status = Value.Check(filterSchema(filterAbles), {$or: 7});
-const status2 = Value.Check(filterSchema(filterAbles), {email: 7});
-console.log(status);
-console.log(status2);
