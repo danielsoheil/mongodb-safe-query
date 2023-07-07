dea5c5d55776914c81b52572b0785814ffb1a977
Daniel <daniel@sohe.ir>
2023-07-07 13:58:50 +0330

    test: added some tests

---
+++ jest.config.js
@@ -0,0 +1,20 @@
+/* eslint-disable @typescript-eslint/no-var-requires */
+const { readFileSync } = require("node:fs");
+
+const config = JSON.parse(readFileSync("./.swcrc", "utf-8"));
+
+/**
+ * @type {import('jest').Config}
+ */
+module.exports = {
+  testEnvironment: "node",
+  setupFiles: ["dotenv/config"],
+  reporters: ["default", "jest-junit"],
+  coverageReporters: ["cobertura"],
+  transform: {
+    "^.+\\.(t|j)sx?$": ["@swc/jest", config],
+  },
+  moduleNameMapper: {
+    "$src/(.*)": "<rootDir>/src/$1",
+  },
+};
---
+++ src/test.ts
@@ -0,0 +1,71 @@
+import { afterEach, beforeEach, expect, it } from "@jest/globals";
+
+import Ajv from "ajv";
+import { filterSchema } from "./index";
+
+const ajv = new Ajv({ removeAdditional: "all" });
+
+const validateWithoutRemoveField = (schema, data) => {
+  const dataBackup = structuredClone(data);
+  expect(ajv.validate(schema, data)).toBe(true);
+  expect(data).toMatchObject(dataBackup);
+};
+
+it("should validate a simple where mongo query with our schema", function () {
+  const schema = filterSchema(["firstName", "lastName"]);
+  validateWithoutRemoveField(schema, {
+    firstName: "daniel",
+    lastName: "soheil",
+  });
+  validateWithoutRemoveField(schema, {
+    lastName: "soheil",
+    firstName: "daniel",
+  });
+  validateWithoutRemoveField(schema, { firstName: "daniel" });
+  validateWithoutRemoveField(schema, { lastName: "soheil" });
+});
+
+it("should validate and remove additional fields", function () {
+  const schema = filterSchema(["firstName"]);
+  {
+    // removeAdditional
+    const data = { firstName: "daniel", age: 12 };
+    expect(ajv.validate(schema, data)).toBe(true);
+    expect(data).toMatchObject({ firstName: "daniel" });
+  }
+});
+
+it("should validate $like operator of where mongo query with our schema", function () {
+  validateWithoutRemoveField(filterSchema(["firstName"]), {
+    firstName: { $like: "%daniel%" },
+  });
+});
+
+it("should validate $eq operator of where mongo query with our schema", function () {
+  validateWithoutRemoveField(filterSchema(["firstName"]), {
+    firstName: { $eq: "daniel" },
+  });
+});
+
+it("should not validate with wrong operator", function () {
+  const data = { firstName: { $xxxxxxx: "daniel" } };
+  expect(ajv.validate(filterSchema(["firstName"]), data)).toBe(false);
+});
+
+it("should remove unexpected field", function () {
+  const data = { xxxxxxx: "daniel" };
+  ajv.validate(filterSchema(["firstName"]), data);
+  expect(data).toMatchObject({});
+});
+
+it("should validate $and operator of where mongo query with our schema", function () {
+  validateWithoutRemoveField(filterSchema(["firstName", "lastName"]), {
+    $and: [{ firstName: "daniel" }, { lastName: "soheil" }],
+  });
+});
+
+it("should validate $or operator of where mongo query with our schema", function () {
+  validateWithoutRemoveField(filterSchema(["firstName", "lastName"]), {
+    $or: [{ firstName: "daniel" }, { lastName: "soheil" }],
+  });
+});
